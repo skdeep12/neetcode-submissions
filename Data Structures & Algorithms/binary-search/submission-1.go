3b6a18bbfc508f5760c4cda3698b65b716e5f86c
Subhash Kumar Samota <subhashsamota12@gmail.com>
2026-09-06 20:24:30 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+	l := 0
+	r := len(nums)-1
+	for l < r {
+		m := (r+l)/2
+		if target < nums[m] {
+			r = m
+		} else if target > nums[m] {
+			l = m+1
+		} else {
+			return m
+		}
+	}
+	if nums[l] == target {
+		return l
+	}
+	return -1
+}
