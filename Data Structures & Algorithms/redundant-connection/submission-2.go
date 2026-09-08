7cd966d4bb10ab67a58aa3c88ca9db06e50956af
Subhash Kumar Samota <subhashsamota12@gmail.com>
2026-09-08 22:54:49 +0530

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.go
@@ -0,0 +1,30 @@
+func findRedundantConnection(edges [][]int) []int {
+	n := len(edges)
+    parent := make([]int, n+1)
+	for i:=0;i<n+1;i+=1{
+		parent[i] = i
+	}
+	for _, e := range edges {
+		if union(&parent, e[0], e[1]) {
+			return e
+		}
+	}
+	return []int{}
+}
+
+func union(parent *[]int, u, v int) bool {
+	up := find(*parent, u)
+	vp := find(*parent, v)
+	if up == vp{
+		return true
+	}
+	(*parent)[vp] = up
+	return false
+}
+
+func find(parent []int, v int) int{
+	if v != parent[v] {
+		parent[v] = find(parent, parent[v])
+	}
+	return parent[v]
+}
